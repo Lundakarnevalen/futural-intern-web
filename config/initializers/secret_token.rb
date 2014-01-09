6fe4c22184f48b4c5b3cff127121d19b9e966839
Johan Förberg <johan@forberg.se>
2014-01-09 15:54:40 +0000

    Init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Futural::Application.config.secret_key_base = '69a822c5561bc8ae48386539dd4b8cd68dfb3391834a643cd80ee42fdf01f64036c41245320893036ab96d09954a553e45baca27c8ba391500f7629dd79fa4fa'
