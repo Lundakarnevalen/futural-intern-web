8b06e8fa1e828d1ae3057afc4ee89c1076ac0fb6
Tomas Nilsson <tomas.nilsson92@gmail.com>
2014-01-26 19:29:26 +0100

    New db migrate file

--- db/migrate/20140126180210_add_details_to_karnevalister.rb
+++ db/migrate/20140126180210_add_details_to_karnevalister.rb
@@ -1,6 +1,6 @@
 class AddDetailsToKarnevalister < ActiveRecord::Migration
   def change
     change_column :karnevalister, :google_token, :text, :limit => nil
-    change_column :karnevalister, :utcheckad, :integer
+    change_column :karnevalister, :utcheckad, 'integer USING CAST(column_to_change AS integer)'
   end
 end
