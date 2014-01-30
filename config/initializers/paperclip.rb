4a73d01e7259b94442bb8d9b4a44134cc7c1ea1a
Lars Nyström <nystrom.lars.egon@gmail.com>
2014-01-30 21:32:59 +0100

    Revert "Potential bug-fix for Paperclip S3"
    This reverts commit 934da2bea8872a2012a173268476db9a5e3708ce.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,1 @@
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_host_name] = 'karnevalist.se.s3-website-eu-west-1.amazonaws.com'
