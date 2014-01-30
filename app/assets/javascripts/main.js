bcfb6adaf60b883628de7a96413928ed3282abdd
Carl Bergman <bergman.carl@gmail.com>
2014-01-30 21:58:53 +0100

    Mer jobb med validering och css

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,7 +3,16 @@ $(function() {
 	var form = $('form');
 	if (form.length) {
 		form.each(function() {
-			$(this).validate();
+			$(this).validate({
+				rules: {
+					'karnevalist[kon_id]': {
+						required: true
+					},
+					'karnevalist[storlek_id]': {
+						required: true
+					}
+				}
+			});
 		});
 	}
 
