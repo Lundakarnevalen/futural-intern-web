a90a2e641794fa9d8e955a7dd394ca3138802886
Lars Nyström <nystrom.lars.egon@gmail.com>
2014-01-31 15:47:12 +0100

    Cache-control for S3 images

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -9,5 +9,5 @@ CarrierWave.configure do |config|
   }
   config.fog_directory  = ENV['S3_BUCKET_NAME']                     # required
   config.fog_public     = false                                   # optional, defaults to true
-  # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
