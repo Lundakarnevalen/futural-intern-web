7dcc0d56ee9165534d8f59b17a7388178f1842ce
Tomas Nilsson <tomas.nilsson92@gmail.com>
2014-01-31 01:49:53 +0100

    Remove message_type from notifications in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140130231214) do
+ActiveRecord::Schema.define(version: 20140131004619) do
 
   create_table "intressen", force: true do |t|
     t.string "name", null: false
@@ -85,7 +85,6 @@ ActiveRecord::Schema.define(version: 20140130231214) do
     t.datetime "updated_at"
     t.string   "title"
     t.text     "message"
-    t.integer  "message_type"
   end
 
   create_table "sektioner", force: true do |t|
