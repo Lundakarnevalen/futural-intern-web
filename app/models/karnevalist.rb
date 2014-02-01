ab98e73a0d2b9782086048457c2f633af33fa719
Tomas Nilsson <tomas.nilsson92@gmail.com>
2014-02-01 13:51:43 +0100

    New checkout pages (digital and paper) - should solve most of #13

--- app/models/karnevalist.rb
+++ app/models/karnevalist.rb
@@ -9,7 +9,6 @@ class Karnevalist < ActiveRecord::Base
   belongs_to :korkort
   belongs_to :user
   accepts_nested_attributes_for :user
-  belongs_to :phone
 
   mount_uploader :foto, FotoUploader
 
