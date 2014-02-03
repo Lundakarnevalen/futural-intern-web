02d09d34e780c5508569d8c41f05f061dd4b4734
Tomas Nilsson <tomas.nilsson92@gmail.com>
2014-02-03 22:17:01 +0100

    AJAX for uppdelning

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,46 @@
 // = require main
 //= require jquery.ui.all
 //= require jquery.modal
+
+function tilldelad_sektion_submit(karnevalist_id) {
+    (function($){
+        $.ajax({
+            url: '/karnevalister/'+karnevalist_id,
+            type: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify({"karnevalist": {"tilldelad_sektion": $("#select"+karnevalist_id+" option:selected").val()}}),
+            dataType: 'json',
+            success: function(data) {
+                $("tr#"+karnevalist_id).addClass("yellow");
+                if ($("#checkbox"+karnevalist_id).attr('disabled')) {
+                    $("#checkbox"+karnevalist_id).removeAttr("disabled");
+                }
+            },
+        }); 
+    })(jQuery);
+}
+
+function tilldelad_klar_submit(karnevalist_id) {
+    (function($){
+        $.ajax({
+            url: '/karnevalister/'+karnevalist_id,
+            type: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify({"karnevalist": {"tilldelad_klar": $("#checkbox"+karnevalist_id).prop('checked')}}),
+            dataType: 'json',
+            success: function(data) {
+                if ($("#checkbox"+karnevalist_id).prop('checked')) {
+                    $("tr#"+karnevalist_id).removeClass("yellow");
+                    $("tr#"+karnevalist_id).addClass("green");
+                    $("#select"+karnevalist_id).attr('disabled', 'disabled');
+                } else {
+                    $("tr#"+karnevalist_id).removeClass("green");
+                    $("tr#"+karnevalist_id).addClass("yellow");
+                    if ($("#select"+karnevalist_id).attr('disabled')) {
+                        $("#select"+karnevalist_id).removeAttr("disabled");
+                    }
+                }
+            },
+        }); 
+    })(jQuery);
+}
