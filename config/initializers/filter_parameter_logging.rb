83862a729ffbfe6722601e72ca78b64ccc81751b
Lars Nyström <nystrom.lars.egon@gmail.com>
2014-02-06 18:25:38 +0100

    Log User Agent

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,3 +2,5 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [:password, :image_data]
+
+Rails.application.config.log_tags = [ lambda { |request| request.user_agent } ]
