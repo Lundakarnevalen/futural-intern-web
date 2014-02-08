70ce743aa1bdaf724f93a2f4dad99403eca9dcea
Lars Nyström <nystrom.lars.egon@gmail.com>
2014-02-08 04:11:37 +0100

    Redigera fält efter steg 2

--- app/controllers/karnevalister_controller.rb
+++ app/controllers/karnevalister_controller.rb
@@ -326,7 +326,7 @@ class KarnevalisterController < ApplicationController
       elsif @karnevalist.avklarat_steg == 1
         redirect_to action: 'step3', id: @karnevalist.id unless action_name == 'step3'
       elsif @karnevalist.avklarat_steg == 2
-        redirect_to action: 'step4', id: @karnevalist.id unless action_name == 'step4'
+        redirect_to action: 'step4', id: @karnevalist.id unless action_name == 'step4' or action_name == 'step3'
       elsif @karnevalist.utcheckad
         redirect_to action: 'step4', id: @karnevalist.id unless action_name == 'step4'
       end
