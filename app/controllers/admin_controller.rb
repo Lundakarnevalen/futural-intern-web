6af52a2e4f191566a4f93ed2177087a430a5d98a
Tomas Nilsson <tomas.nilsson92@gmail.com>
2014-02-16 22:12:03 +0100

    Problem with admin_controller when deploying to Heroku.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,19 +2,19 @@ class AdminController < ApplicationController
   before_filter :authenticate_user!
 
   def dump
-    con = ActiveRecord::Base.connection
-    data = con.tables.map{ |table|
-      result = con.exec("select * from `#{table}`")
-      fields = result.fields
-      [table, entries.map{ |entry|
-        fields.zip(entry)
-      }.inject({}) { |acc, pair|
-        acc[pair.first] = pair.last
-      }
-    }.inject({}) {|acc, pair|
-      acc[pair.first] = pair.last
-    }
-    render :json => data
+    #con = ActiveRecord::Base.connection
+    #data = con.tables.map{ |table|
+    #  result = con.exec("select * from `#{table}`")
+    #  fields = result.fields
+    #  [table, entries.map{ |entry|
+    #    fields.zip(entry)
+    #  }.inject({}) { |acc, pair|
+    #    acc[pair.first] = pair.last
+    #  }
+    #}.inject({}) {|acc, pair|
+    #  acc[pair.first] = pair.last
+    #}
+    #render :json => data
   end
 end
     
