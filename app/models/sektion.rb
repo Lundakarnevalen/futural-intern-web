f56820b0d5270e668acac0ad1c9f5c198dab4d8a
Johan Förberg <johan@forberg.se>
2014-02-18 20:38:48 +0100

    Excel export av sektioner fixat.

--- app/models/sektion.rb
+++ app/models/sektion.rb
@@ -1,3 +1,7 @@
 class Sektion < ActiveRecord::Base
   has_and_belongs_to_many :karnevalister
+
+  def members
+    Karnevalist.where :tilldelad_sektion => self.id
+  end
 end
--- app/models/storlek.rb
+++ app/models/storlek.rb
@@ -1,3 +1,7 @@
 class Storlek < ActiveRecord::Base
   has_many :karnevalister
+
+  def to_s
+    self.name
+  end
 end
