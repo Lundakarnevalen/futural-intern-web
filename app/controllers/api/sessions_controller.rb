e09c7ede2585016af4cefc03f008d4128f40f91f
drowzyoriginal <simon.thornqvist@gmail.com>
2014-03-21 00:37:15 +0100

    api test controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,14 +1,13 @@
 class Api::SessionsController < Api::AuthenticationController
   prepend_before_filter :require_no_authentication, only: :create
   before_filter :validate_auth_token, except: :create
-  include Devise::Controllers::Helpers
 
   def create
     resource = User.find_for_database_authentication(email: params[:user][:email])
     return invalid_credentials unless resource
 
     if resource.valid_password?(params[:user][:password])
-      sign_in(:user, resource)
+      sign_in(:user, resource, store: false)
       resource.ensure_authentication_token
       render json: { success: true, token: resource.authentication_token, karnevalist: resource.karnevalist }
       return
@@ -17,7 +16,7 @@ class Api::SessionsController < Api::AuthenticationController
   end
 
   def destroy
-    resource.reset_authentication_token!
+    @resource.reset_authentication_token!
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
     render status: 200, :json => {}
   end
@@ -25,11 +24,6 @@ class Api::SessionsController < Api::AuthenticationController
   private
     def invalid_credentials
       return render json: { success: false, errors: [t('Incorrect email or password')] }, status: :unauthorized
-    end
-
-    def validate_auth_token
-      self.resource = User.find_by_authentication_token(params[:token])
-      render status: :unauthorized, json: {errors: [t('invalid token')]} if self.resource.nil?
     end
 
 end
--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -2,22 +2,17 @@ require "spec_helper"
 describe "routes" do
   describe "routes for Devise" do
     it "routes /api/users/sign_in to the sessions controller" do
-      { :get => "/api/users/sign_in" }.
-        should route_to(:controller => "api/sessions", :action => "new")
+      { get: "/api/users/sign_in" }.
+        should route_to(controller: "api/sessions", action: "new")
     end
 
     it "routes /api/users/sign_out to the sessions controller" do
-      { :delete => "/api/users/sign_out" }.
-        should route_to(:controller => "api/sessions", :action => "destroy")
+      { delete: "/api/users/sign_out" }.
+        should route_to(controller: "api/sessions", action: "destroy")
     end
 
     it "routes /api/users/password/new to the passwords controller" do
-      { :get => "/api/users/password/new" }.should route_to(:controller => "api/passwords", :action => "new")
-    end
-  end
-  describe "phones" do
-    it "routes /phones to /api/phones" do
-      {post: '/phones' }.should route_to(controller: "api/phones", action: "create")
+      { get: "/api/users/password/new" }.should route_to(controller: "api/passwords", action: "new")
     end
   end
 end
