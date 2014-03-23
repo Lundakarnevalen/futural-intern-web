9ae610802b7c4ae0c98b0acf3cce3d8758d6870d
drowzyorginal <simon.thornqvist@gmail.com>
2014-03-23 13:25:17 +0100

    warehouse model spec, warehouse model factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -98,4 +98,31 @@ FactoryGirl.define do
   factory :sync do
   end
 
+  factory :order do
+    status "delivered"
+    order_date Date.today
+    deliver_date Date.tomorrow
+    comment "No comment"
+    association :karnevalist, factory: :karnevalist
+  end
+
+  factory :product_category do
+    name "tools"
+  end
+
+  factory :product do
+    product_type 1
+    association :product_category, factory: :product_category
+    name "hammare"
+    unit "kg"
+    ean "12345678910"
+    supplier "bauhaus"
+    info "information"
+    stock_location "Lund"
+    notes "en bra hammare"
+    stock_balance_ordered 100
+    stock_balance_not_ordered 200
+    stock_balance_stand_by 50
+    purchase_price 25.50
+  end
 end
