80cfd828e1ee37a1dfcce3d4eb9049e5f2445695
Per-Victor Persson <dracyr@gmail.com>
2014-03-25 17:27:51 +0100

    Posts now belongs to a sektion properly

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,17 +2,11 @@ class HomeController < ApplicationController
   skip_authorization_check
 
   def index
-    unless signed_in?
+    if signed_in?
+      @sektion = current_user.karnevalist.sektion
+      @posts = @sektion.posts
+    else
       redirect_to new_karnevalist_path
     end
-    @sektion = Sektion.find(1)
-    @posts = Sektion.find(1).posts
   end
-
-  def show
-    #@sektion = Karnevalist.find(params[:id]).sektion
-    @sektion = Sektion.find(1)
-    @posts = @sektion.posts.paginate(page: params[:page])
-  end
-
 end
