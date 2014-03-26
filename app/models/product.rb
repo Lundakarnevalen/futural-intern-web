edeb860bff55d85ec445411531824826a1813652
Tomas Nilsson <tomas.nilsson92@gmail.com>
2014-03-26 18:26:06 +0100

    Small fixes

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   has_many :order_products
   has_many :orders, through: :order_products
 
-  def product_info(order_id)
-    return self.order_products.find_by_order_id(order_id)
+  def amount(order_id)
+    return self.order_products.find_by_order_id(order_id).amount
   end
 end
