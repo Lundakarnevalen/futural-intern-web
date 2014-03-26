c3d3f0d09203467a4fad7b89b8ad592a48403e86
Felix <felixhedlund@me.com>
2014-03-26 20:45:02 +0100

    Show prices for orders

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,8 @@ class Product < ActiveRecord::Base
   def amount(order_id)
     return self.order_products.find_by_order_id(order_id).amount
   end
+
+  def total_price(amount, unit_price)
+    return amount*unit_price
+  end
 end
