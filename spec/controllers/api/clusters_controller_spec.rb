398f29100bc0f8586ece942ecbe87dff47d5911b
drowzyorginal <simon.thornqvist@gmail.com>
2014-03-26 06:38:28 +0100

    update clusters, remove previous points if quantity == 1

--- spec/controllers/api/clusters_controller_spec.rb
+++ spec/controllers/api/clusters_controller_spec.rb
@@ -53,6 +53,43 @@ describe Api::ClustersController do
   end
 
   describe "PUT to API::ClustersController with :id" do
+    describe "a cluster with quantity > 1" do
+      before :each do
+        @c = FactoryGirl.create(:cluster, lat: 50.10, lng: 50.10, quantity: 10)
+      end
+
+      it "should create a new cluster if no matches are found" do
+        put :update, { token: @user.authentication_token, cluster: { lat: 35.17, lng: -137.50 }, id: @c.id }, format: :json
+        response.code.should eq("200")
+        response.body['cluster_id'].should_not be_nil
+        Cluster.all.length.should eq(2)
+      end
+
+      it "should not increament the quantity if the cordinates are the same as previously" do
+        put :update, { token: @user.authentication_token, cluster: { lat:50.10, lng:50.10 }, id: @c.id }, format: :json
+        response.code.should eq("200")
+        response.body['cluster_id'].should_not be_nil
+        @c.reload
+        @c.quantity.should eq(10)
+      end
+
+      it "should decrement the previous cluster if it is no longer included" do
+        put :update, { token: @user.authentication_token, cluster: { lat: 15.15, lng: -35.52 }, id: @c.id }, format: :json
+        response.code.should eq("200")
+        response.body['cluster_id'].should_not be_nil
+        @c.reload
+        @c.quantity.should eq(9)
+      end
+    end
+
+    it "should delete the previous cluster if quantity = 1" do
+      c = FactoryGirl.create(:cluster, lat: 50.15, lng: -173.203, quantity: 1)
+      id = c.id
+      put :update, { token: @user.authentication_token, cluster: { lat: 15.15, lng: -35.52 }, id: id }, format: :json
+      response.code.should eq("200")
+      response.body['cluster_id'].should_not be_nil
+      expect { Cluster.find(id) }.to raise_error
+    end
   end
 
 end
