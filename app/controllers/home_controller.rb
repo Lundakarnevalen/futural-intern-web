acc96fe0fc0668279cb65fd3fa50927921d1fe22
Per-Victor Persson <dracyr@gmail.com>
2014-03-29 18:08:14 +0100

    Can Create/Edit/Delete Posts from web

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,7 @@ class HomeController < ApplicationController
       unless current_user.karnevalist.sektion.nil?
         @sektioner = current_user.karnevalist.tilldelade_sektioner
         @posts = @sektioner.map { |s| s.posts }.flatten
+        @post = current_user.karnevalist.sektion.posts.build
       end
     else
       redirect_to new_karnevalist_path
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+    #before_action :sign_in
+    skip_authorization_check
+
+    def new
+      @tilldelade_sektioner = current_user.karnevalist.tilldelade_sektioner
+      @post = Post.new
+    end
+
+    def create
+        p = params[:post]
+        sektion_id = p[:sektion].to_i
+        p[:sektion] = Sektion.find(sektion_id)
+        p[:karnevalist] = current_user.karnevalist
+        @post = Post.new(p)
+        if @post.save
+          flash[:success] = "Inlägg skapat!"
+        end
+        redirect_to root_url
+    end
+
+    def edit
+      @post = current_user.karnevalist.sektion.posts.find_by(id: params[:id])
+    end
+
+    def update
+      @post = current_user.karnevalist.sektion.posts.find_by(id: params[:id])
+      p = params[:post]
+      sektion_id = p[:sektion].to_i
+      p[:sektion] = Sektion.find(sektion_id)
+      if @post.update_attributes(p)
+        flash[:success] = "inlägg redigerat"
+      end
+      redirect_to root_url
+    end
+
+    def destroy
+      post = current_user.karnevalist.sektion.posts.find_by(id: params[:id])
+      post.destroy
+      redirect_to root_url
+    end
+
+    private
+
+      def post_params
+        params.require(:post).permit(:title, :content, :sektion)
+      end
+end
