932138b2bd3322efa22e7a79d78f82ef0bf6b240
Tomas Nilsson <tomas.nilsson92@gmail.com>
2014-03-30 10:32:03 +0200

    Check if current user is karnevalist in home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   skip_authorization_check
 
   def index
-    if signed_in?
+    if signed_in? && current_user.karnevalist?
       unless current_user.karnevalist.tilldelade_sektioner.blank?
         @sektioner = current_user.karnevalist.tilldelade_sektioner
         #@posts = @sektioner.map { |s| s.posts }.flatten
