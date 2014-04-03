8ef5f8fbb947d75aeb8658097e7d94dbc916c3d8
drowzyoriginal <simon.thornqvist@gmail.com>
2014-04-03 02:12:45 +0200

    Update scoped routes

--- app/controllers/warehouse/application_controller.rb
+++ app/controllers/warehouse/application_controller.rb
@@ -1,4 +1,11 @@
 class Warehouse::ApplicationController < ApplicationController
   # TODO only for dev
+  before_filter :w_code
   skip_authorization_check
+
+  def w_code
+    return @warhouse_code = 0  if /fabriken/.match(request.fullpath)
+    return @Warehouse_code = 1 if /festmasteriet/.match(request.fullpath)
+    @Warehouse_code = -1
+  end
 end
