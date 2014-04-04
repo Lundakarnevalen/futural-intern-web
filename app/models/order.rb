866d6c092693c864bfc300c22e049f9bf45aa3a7
Tomas Nilsson <tomas.nilsson92@gmail.com>
2014-04-05 01:24:53 +0200

    Warehouse calendar update

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,6 +12,10 @@ class Order < ActiveRecord::Base
   def set_order_date
     self.order_date = DateTime.now if self.order_date.blank?
   end
+
+  def start_time
+    self.delivery_date
+  end
   
   def total_sum
     sum = 0;
