7de9f11bafa0ef67ba4a1d2dc9b6abf7c75e28be
Tomas Nilsson <tomas.nilsson92@gmail.com>
2014-04-06 20:44:13 +0200

    Home controller spec update

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20140404013529) do
     t.datetime "updated_at"
   end
 
+  add_index "clusters", ["lat", "lng"], name: "index_clusters_on_lat_and_lng"
+
   create_table "incoming_deliveries", force: true do |t|
     t.string   "invoice_nbr"
     t.integer  "warehouse_code"
@@ -29,8 +31,6 @@ ActiveRecord::Schema.define(version: 20140404013529) do
     t.datetime "updated_at"
   end
 
-  add_index "clusters", ["lat", "lng"], name: "index_clusters_on_lat_and_lng"
-  
   create_table "incoming_deliveries_karnevalister", id: false, force: true do |t|
     t.integer "karnevalist_id"
     t.integer "incoming_delivery_id"
@@ -100,8 +100,8 @@ ActiveRecord::Schema.define(version: 20140404013529) do
     t.boolean  "pusseldag_keep"
     t.integer  "podio_id"
     t.boolean  "medlem_kollad",         default: false
-    t.integer  "tilldelad_sektion2"
     t.text     "ios_token"
+    t.integer  "tilldelad_sektion2"
   end
 
   add_index "karnevalister", ["efternamn"], name: "index_karnevalister_on_efternamn"
@@ -176,6 +176,8 @@ ActiveRecord::Schema.define(version: 20140404013529) do
     t.datetime "updated_at"
   end
 
+  add_index "phones", ["google_token"], name: "index_phones_on_google_token", unique: true
+
   create_table "podio_syncs", force: true do |t|
     t.datetime "time"
   end
@@ -190,7 +192,7 @@ ActiveRecord::Schema.define(version: 20140404013529) do
   end
 
   add_index "posts", ["sektion_id", "karnevalist_id", "created_at"], name: "index_posts_on_sektion_id_and_karnevalist_id_and_created_at"
-  
+
   create_table "product_categories", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
