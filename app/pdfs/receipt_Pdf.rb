5bfeea209f4961e7bf76195689f066a76a5966a9
Felix <felixhedlund@me.com>
2014-04-06 14:04:49 +0200

    Product name in Receipt

--- app/pdfs/receipt_Pdf.rb
+++ app/pdfs/receipt_Pdf.rb
@@ -3,6 +3,7 @@ class ReceiptPdf < Prawn::Document
   def initialize(order, view, warehouse_code)
     super()
     @order = order
+    products = @order.products
     if warehouse_code == 0
       @warehouse = "Fabriken"
       @image_path = "app/pdfs/fabriken.png"
@@ -19,6 +20,12 @@ class ReceiptPdf < Prawn::Document
     text "Beställningsdatum: #{order_date}"
     !@order.delivery_date.blank? ? collect_date = @order.delivery_date.strftime("%Y-%m-%d") : collect_date = ""
     text "Hämtdatum: #{collect_date}"
+    product_number = 1
+    products.each do |p|
+      order_product = OrderProduct.where(:order_id => @order.id, :product_id => p.id).first
+      text "Produkt #{product_number}: #{p.name}, #{order_product.amount} #{p.unit}"
+      product_number += 1
+    end
     text "Totalpris: #{@order.total_sum} kr"
 
     image @image_path, :position => :center, :scale => 0.7
