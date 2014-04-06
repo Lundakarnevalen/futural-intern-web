d83d33b3045b91cf27ac0631c446e7662ec15481
Per-Victor Persson <dracyr@gmail.com>
2014-04-06 17:40:16 +0200

    Added posts to homepage, layout stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,14 @@
 class HomeController < ApplicationController
   skip_authorization_check
+  layout 'home/application'
 
   def index
     if signed_in?
-      if current_user.karnevalist? && 
+      if current_user.karnevalist? &&
          !current_user.karnevalist.tilldelade_sektioner.blank?
         @sektioner = current_user.karnevalist.tilldelade_sektioner
+        @posts = @sektioner.map { |s| s.posts }.flatten
+        @post = current_user.karnevalist.sektion.posts.build
       end
     else
       redirect_to new_user_session_path
