13f15febd989bd7f5b763097b6c2eac1149e9183
Per-Victor Persson <dracyr@gmail.com>
2014-04-08 19:55:03 +0200

    Layout and InfoPages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,7 @@ class HomeController < ApplicationController
          !current_user.karnevalist.tilldelade_sektioner.blank?
         @sektioner = current_user.karnevalist.tilldelade_sektioner
         @posts = @sektioner.map { |s| s.posts }.flatten
+        @posts.sort_by! { |p| p.created_at }.reverse!
         @post = current_user.karnevalist.sektion.posts.build
         if @posts.empty?
           @posts << Post.new(title: "Hoppsan!", content: "Din sektions kommunikationsnisse har inte lagt upp något här än :(", sektion: @sektioner.first)
