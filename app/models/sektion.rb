ca7dbfcf82411ccb46f0782b78366b7a03ea4a8a
Per-Victor Persson <dracyr@gmail.com>
2014-04-10 17:10:20 +0200

    Re-Add info page to sektion

--- app/models/sektion.rb
+++ app/models/sektion.rb
@@ -2,6 +2,7 @@ class Sektion < ActiveRecord::Base
   has_and_belongs_to_many :karnevalister
   has_many :posts
   has_many :orders
+  validates :info_page
 
   def members
     Karnevalist.where 'tilldelad_sektion = ? or tilldelad_sektion2 = ?',
