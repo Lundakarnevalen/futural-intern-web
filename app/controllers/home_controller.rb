55070aab654c1dee6695b8ddeb41fd86ac8ae5e6
Tomas Nilsson <tomas.nilsson92@gmail.com>
2014-04-12 02:44:11 +0200

    Android link update - should open in Google Play

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,7 +14,7 @@ class HomeController < ApplicationController
 
   def app_store
     urls = {
-      android: "https://play.google.com/store/apps/details?id=se.lundakarnevalen.android",
+      android: "market://details?id=se.lundakarnevalen.android",
       iphone: "https://itunes.apple.com/se/app/karnevalisten/id811615995?mt=8"
     }
     user_agent = request.user_agent.downcase.match(/android|iphone/).to_s
