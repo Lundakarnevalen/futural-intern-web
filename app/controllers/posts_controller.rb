1cbdcf0e7e559770d68f81c1f94c2a89086df582
Per-Victor Persson <dracyr@gmail.com>
2014-04-13 15:00:24 +0200

    Fixed markdown rendering i just broke

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,7 +46,6 @@ class PostsController < ApplicationController
       p = s.posts.find_by(id: params[:id])
       @post = p unless p.nil?
     end
-    @post_html = render_markdown @post.content
   end
 
   private
