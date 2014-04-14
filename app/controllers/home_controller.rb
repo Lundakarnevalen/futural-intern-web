87342e56e0e71a82c814bf5f4b75bfe68df50847
Per-Victor Persson <dracyr@gmail.com>
2014-04-14 15:55:41 +0200

    More checks for user missing karnevalist

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,13 +6,10 @@ class HomeController < ApplicationController
     if signed_in?
       if current_user.karnevalist? &&
          !current_user.karnevalist.tilldelade_sektioner.blank?
-        @sektioner = current_user.karnevalist.tilldelade_sektioner
+        @sektioner = current_sektioner
         @posts = @sektioner.map { |s| s.posts }.flatten
         @posts.sort_by! { |p| p.created_at }.reverse!
         @post = current_user.karnevalist.sektion.posts.build
-        if @posts.empty?
-          @posts << Post.new(title: "Hoppsan!", content: "Din sektions kommunikationsnisse har inte lagt upp något här än :(", sektion: @sektioner.first)
-        end
       end
     else
       redirect_to new_user_session_path
