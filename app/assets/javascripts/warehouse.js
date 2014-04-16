39571a953f8becac8165af4ddd9c9104f2f6f794
drowzyoriginal <simon.thornqvist@gmail.com>
2014-04-16 09:14:29 +0200

    form, init action show

--- app/assets/javascripts/warehouse.js
+++ app/assets/javascripts/warehouse.js
@@ -1,21 +1,41 @@
 $(function() {
-  $('#bookings').fullCalendar({
+  var $el = $('#bookings');
+  $el.fullCalendar({
     editable: true,
+
     header: {
       left: 'prev, next, today',
       center: 'title',
       right: 'month, agendaWeek ,agendaDay'
     },
+
     defaultView: 'month',
-    height: 600,
-    slotMinutes: 15,
+    height: 700,
+    slotMinutes: 30,
+    minTime: 7,
+    maxTime: 22,
+    hiddenDays: [6],
+
+    slotEventOverlap: false,
+
     eventSources:[{
       url: '/fabriken/reservations.json'
     }],
-    timeFormat: 'h:mm t{ - h:mm t} ',
-    dragOpacity: "0.5",
-    eventClick: function(event, jsEvent, view){
-      showEventDetails(event);
+
+    select: function(start, end, allDay) {
+      $el.fullCalendar('unselect');
     },
+
+    eventRender: function(event, element) {
+    },
+
+    selectable: true,
+    selectHelper: true,
+
+    timeFormat: 'H:mm { - H:mm } ',
+    dragOpacity: "0.5",
+    eventClick: function(event, jsEvent, view) {
+      showEventDetails(event);
+    }
   });
 });
