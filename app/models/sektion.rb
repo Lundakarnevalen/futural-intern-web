a115752587906f7b5ba123ed2069669f5d177b64
Per-Victor Persson <dracyr@gmail.com>
2014-04-18 11:42:16 +0200

    Can edit contact and english pages

--- app/models/sektion.rb
+++ app/models/sektion.rb
@@ -3,7 +3,6 @@ class Sektion < ActiveRecord::Base
   has_and_belongs_to_many :karnevalister
   has_many :posts
   has_many :orders
-  attr_accessor :english_page, :contact_page, :info_page
 
   def members
     Karnevalist.where 'tilldelad_sektion = ? or tilldelad_sektion2 = ?',
--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,9 @@
       get ':id/aktiva', :to => 'sektioner#aktiva'
       post ':id/edit', :to => 'sektioner#update'
       get ':id/contact', :to => 'sektioner#show_contact', :as => 'show_contact'
+      post ':id/contact/edit', :to => 'sektioner#edit_contact', :as => 'edit_contact'
       get ':id/english', :to => 'sektioner#show_english', :as => 'show_english'
+      post ':id/english/edit', :to => 'sektioner#edit_english', :as => 'edit_english'
     end
   end
 
