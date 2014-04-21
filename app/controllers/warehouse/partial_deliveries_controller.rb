ea600fdfd27ec6b0edf2e5ef7bc1ed66b2a236ba
Tomas Nilsson <tomas.nilsson92@gmail.com>
2014-04-21 11:15:20 +0200

    Partial delivery now working

--- app/controllers/warehouse/partial_deliveries_controller.rb
+++ app/controllers/warehouse/partial_deliveries_controller.rb
@@ -22,36 +22,21 @@ class Warehouse::PartialDeliveriesController < Warehouse::ApplicationController
 
   def create
     @partial_delivery = PartialDelivery.new(partial_delivery_params)
-    @partial_delivery.karnevalister.push current_user.karnevalist
-    @partial_delivery.warehouse_code = @warehouse_code
-    if params[:finished]
-      @partial_delivery.ongoing = false
-    else
-      @partial_delivery.ongoing = true
-    end
+    @partial_delivery.seller_id = current_user.karnevalist.id
     if @partial_delivery.save
-      if !@partial_delivery.ongoing
-        @partial_delivery.partial_delivery_products.each do |partial_delivery|
-          product = Product.find(partial_delivery.product_id)
-          stand_by = product.stock_balance_stand_by
-          if stand_by == 0
-            stock_balance_not_ordered = product.stock_balance_not_ordered + partial_delivery.amount.to_i
-            product.update_attributes(:stock_balance_not_ordered => stock_balance_not_ordered)   
-          elsif stand_by >= partial_delivery.amount.to_i
-            stock_balance_ordered = product.stock_balance_ordered + partial_delivery.amount.to_i
-            stand_by -= partial_delivery.amount.to_i
-            product.update_attributes(:stock_balance_ordered => stock_balance_ordered)
-            product.update_attributes(:stock_balance_stand_by => stand_by)
-          else
-            stock_balance_ordered = product.stock_balance_ordered + stand_by
-            stock_balance_not_ordered = partial_delivery.amount.to_i - stand_by
-            product.update_attributes(:stock_balance_ordered => stock_balance_ordered)
-            product.update_attributes(:stock_balance_not_ordered => stock_balance_not_ordered)
-            product.update_attributes(:stock_balance_stand_by => 0)
-          end
+      order = Order.find(@partial_delivery.order_id)
+      @partial_delivery.partial_delivery_products.each do |partial_product|
+        product = Product.find(partial_product.product_id)
+        in_stock = product.stock_balance_ordered
+        if in_stock >= partial_product.amount.to_i
+          in_stock -= partial_product.amount.to_i
+          product.update_attributes(:stock_balance_ordered => in_stock)
+          order_product = product.order_products.find_by_order_id(order.id)
+          delivered_amount = order_product.delivered_amount + partial_product.amount.to_i
+          order_product.update_attributes(:delivered_amount => delivered_amount)
         end
       end
-      redirect_to partial_delivery_path(@partial_delivery)
+      redirect_to order_path(params[:partial_delivery][:order_id])
     else
       @product_categories = ProductCategory.where(warehouse_code: @warehouse_code).order("name ASC")
       @partial_delivery.partial_delivery_products.build
