b821cf5946f343f38e27a048265aaeacb9d51322
drowzyoriginal <simon.thornqvist@gmail.com>
2014-04-26 17:17:36 +0200

    init photo model

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -13,4 +13,11 @@ CarrierWave.configure do |config|
     config.fog_public     = true                                   # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   end
+  if Rails.env.test? || Rails.env.development?
+    config.storage = :file
+    config.enable_processing = false
+    config.root = "#{Rails.root}/tmp"
+  else
+    config.storage = :fog
+  end
 end
