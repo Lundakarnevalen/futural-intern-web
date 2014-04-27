2eead2bf5e29062086499579c42b150e00085126
Felix <felixhedlund@me.com>
2014-04-27 11:03:42 +0200

    POST is working

--- app/controllers/bookkeepings_controller.rb
+++ app/controllers/bookkeepings_controller.rb
@@ -8,4 +8,19 @@ class BookkeepingsController < ApplicationController
   def new
     @bookkeeping = current_user.karnevalist.bookkeepings.new
   end
+
+  def create
+    @bookkeeping = current_user.karnevalist.bookkeepings.new(bookkeeping_params)
+    @bookkeeping.karnevalist_id = current_user.karnevalist.id
+    if @bookkeeping.save
+      redirect_to action: "index"
+    else
+      render :new
+    end
+  end
+  private
+    def bookkeeping_params
+      params.require(:bookkeeping).permit( :karnevalist_id, :question_1, :question_2, :question_3, :question_4 )
+    end
+  
 end
