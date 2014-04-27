f1134f4974abb6b08179d69524f95045b9e0fdbd
drowzyoriginal <simon.thornqvist@gmail.com>
2014-04-28 00:25:04 +0200

    added create for photos controller

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -1,8 +1,7 @@
 class Api::PhotosController < Api::ApiController
   before_filter :authenticate_user_from_token!
-
   def index
-    photos = Photo.where(accepted: true).all
+    photos = Photo.where(accepted: true)
     render status: 200, json: { success: true, photos: photos}
   end
 
@@ -12,15 +11,18 @@ class Api::PhotosController < Api::ApiController
   end
 
   def create
-  end
-
-  def update
-  end
-
-  def delete
+    p = photo_params
+    p.merge!(karnevalist_id: current_user.karnevalist.id)
+    @photo = Photo.new(p)
+    render_response(@photo.save)
   end
 
   private
+    def render_response(success)
+      return render status: 400, json: { success: false, errors: @photo.errors.full_messages } unless success
+      render status: 201, json: { success: true, photo: @photo }
+    end
+
     def photo_params
       params.require(:photo).permit!
     end
