19700acedf0b5f1a270673f58285c88ac4c548b9
Johan Förberg <johan@forberg.se>
2014-04-28 21:08:25 +0200

    Correct migration for karneblocket.

--- db/migrate/20140424115129_create_ticket_listings.rb
+++ db/migrate/20140424115129_create_ticket_listings.rb
@@ -1,7 +1,11 @@
 class CreateTicketListings < ActiveRecord::Migration
   def change
     create_table :ticket_listings do |t|
-
+      t.text :description
+      t.integer :price
+      t.boolean :selling, :default => true # true: sell, false: trade
+      t.integer :user_id
+      t.integer :event_id
       t.timestamps
     end
   end
--- db/migrate/20140428184609_add_strings_to_ticket_listings.rb
+++
@@ -1,6 +0,0 @@
-class AddStringsToTicketListings < ActiveRecord::Migration
-  def change
-    add_column :ticket_listings, :title, :string
-    add_column :ticket_listings, :description, :string
-  end
-end
