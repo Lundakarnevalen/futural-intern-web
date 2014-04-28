0871253c2500304aece84dcf93539bc25b6335dc
Erik Bjäreholt <erik.bjareholt@gmail.com>
2014-04-28 22:45:31 +0200

    A bunch of in-progress stuff and new files

--- app/controllers/karneblocket/listings_controller.rb
+++ app/controllers/karneblocket/listings_controller.rb
@@ -1,10 +1,12 @@
+# -*- encoding : utf-8 -*-
 class Karneblocket::ListingsController < ApplicationController
   skip_before_filter :require_login
 
   skip_authorization_check
   def index
     if signed_in?
-
+      #render :json => TicketListing::first()
+      @listings = TicketListing.all
     else
       redirect_to new_user_session_path
     end
@@ -14,6 +16,14 @@ class Karneblocket::ListingsController < ApplicationController
   end
 
   def create
-    render text: params[:listing].inspect
+    @listing = TicketListing.new(params[:ticketlisting])
+    @listing.seller = current_karnevalist
+    @listing.save
+    render :json => {
+        :user => c,
+        :listing => params[:listing],
+        :listing_created => @listing.inspect
+    }
+    #render text: params.inspect + " Karnevalist: " + c.karnevalist.attributes.inspect + " Full user:" + c.attributes.inspect
   end
 end
