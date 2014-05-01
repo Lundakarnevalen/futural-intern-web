377c9cbad3e2767ec811cfb3fe54dd0d0755f1e6
Tomas Nilsson <tomas.nilsson92@gmail.com>
2014-05-01 14:56:43 +0200

    Warehouse mail update

--- app/mailers/warehouse_mailer.rb
+++ app/mailers/warehouse_mailer.rb
@@ -2,7 +2,13 @@
 class WarehouseMailer < ActionMailer::Base
   add_template_helper(Warehouse::ApplicationHelper)
 
-  def notify_delivery(sender, receiver, subject, order)
+  def notify_delivery(sender, receiver, subject, order, warehouse_code)
+    @order = order
+    @warehouse_code = warehouse_code
+    mail(to: receiver, subject: subject, from: sender)
+  end
+
+  def new_order(sender, receiver, subject, order)
     @order = order
     mail(to: receiver, subject: subject, from: sender)
   end
