dd072fd331f5e95595fe057d9fccdfe974fe9cc2
drowzyoriginal <simon.thornqvist@gmail.com>
2014-05-02 22:07:57 +0200

    added caption

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,6 +3,7 @@ class Photo < ActiveRecord::Base
   mount_uploader :image, FotoUploader
   validates :karnevalist, presence: true
   validates_presence_of :image
+  validates_length_of :caption, maximum: 140, allow_blank: true
 
   def as_json(options = {})
     {
