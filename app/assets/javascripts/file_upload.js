5ae0e8b06f353ce72c77c3a9d38b444949b8c5c3
drowzyoriginal <simon.thornqvist@gmail.com>
2014-05-02 13:56:48 +0200

    show modal when clicking picture

--- app/assets/javascripts/file_upload.js
+++ app/assets/javascripts/file_upload.js
@@ -4,4 +4,8 @@ $(function() {
     label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
     input.closest('.input-group-btn').children('.form-control').val(label);
   });
+
+  $('a .img-modal-toggle').click(function(e) {
+    $('#img-modal .modal-body .thumbnail').children('img').attr('src', $(this).attr('src'));
+  });
 });
