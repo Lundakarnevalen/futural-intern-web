7952c34ea91517ff1485e5db266aa14834dbb082
drowzyoriginal <simon.thornqvist@gmail.com>
2014-05-02 21:34:09 +0200

    added success / error alert

--- app/assets/javascripts/karnegram.js
+++ app/assets/javascripts/karnegram.js
@@ -8,9 +8,12 @@ $(function() {
   $('a .img-modal-toggle').click(function(e) {
     $('#img-modal .modal-body .thumbnail').children('img').attr('src', $(this).attr('src'));
   });
+  var accMsg = 'Fotot godkändes',
+      denyMsg = 'Fotot togs bort.';
 
-  function changePhotoStatus(action) {
+  function changePhotoStatus(action, msg) {
     var id = $(this).data('id'),
+        $status = $('#photo-status'),
         data = action === 'POST' ? { "_method": "delete" } : { photo: { accepted: true} };
     $.ajax({
       type: action,
@@ -19,17 +22,25 @@ $(function() {
       dataType: 'json',
       success: function() {
         $('#photo-row-' + id).html("");
+        console.log($status);
+        $status
+          .text(msg)
+          .attr('class', 'alert alert-success alert-dismissible')
+          .show();
       },
       error: function() {
-        console.log('Error');
+        $status
+          .text('Operationen kunde inte utföras')
+          .attr('class', 'alert alert-danger alert-dismissible')
+          .show();
       }
     });
   }
 
   $('.accept-photo').click(function(e) {
-    changePhotoStatus.call(this, 'PUT');
+    changePhotoStatus.call(this, 'PUT', accMsg);
    });
   $('.remove-photo').click(function(e) {
-    changePhotoStatus.call(this, 'POST');
+    changePhotoStatus.call(this, 'POST', denyMsg);
   });
 });
