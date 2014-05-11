586095e3d4aa65df5f817fa26ea7104760a3849e
Per-Victor Persson <dracyr@gmail.com>
2014-05-11 11:38:49 +0200

    Can upload pdf to sektionfiles

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,4 +3,8 @@ class Image < ActiveRecord::Base
   belongs_to :sektion
   validates :image, :presence => :true
   validates :name, :presence => :true
+
+  def pdf?
+    self.image_url.to_s.split('.').last == 'pdf'
+  end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,6 +15,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(jpg jpeg png)
+    %w(jpg jpeg png pdf)
   end
 end
