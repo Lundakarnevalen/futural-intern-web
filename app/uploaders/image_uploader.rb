7bab96674e7fdbced3ea9035f57bbc53772131dd
Per-Victor Persson <dracyr@gmail.com>
2014-05-11 12:49:34 +0200

    PDF thumbnails

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -17,4 +18,21 @@ class ImageUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(jpg jpeg png pdf)
   end
+
+  version :pdf_thumb, :if => :is_pdf? do
+    process :resize_to_limit => [250, 250]
+    process :convert => :jpg
+    process :set_content_type
+  end
+
+  private
+
+  def set_content_type(*args)
+    self.file.instance_variable_set(:@content_type, "image/jpg")
+  end
+
+  def is_pdf? picture
+    picture.file.split('.').last == 'pdf'
+  end
+
 end
